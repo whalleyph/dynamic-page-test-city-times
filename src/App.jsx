4d8befa2f4bd5f27a087f545aee364d1449408dd
2405-011-pw <walypa56@gmail.com>
2024-06-12 16:09:27 +0100

    dynamic London time

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css'
-
 function App() {
+  const date = new Date()
+  let hours = date.getHours()
+  const minutes = String(date.getMinutes()).padStart(2, '0');
   return (
     <>
+      <p>The time in London is: {hours}:{minutes}</p>
     </>
   )
 }
